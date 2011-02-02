62e71c886b027ae090f9218f376507e8df3b82d2
Mykhaylo Sorochan <msorc@bigmir.net>
2011-02-02 06:08:52 -0800

    To find Spec::Matchers

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 if Rails.env == "test"
+  require 'spec/matchers'
   require 'spec/rails/matchers/observers'
   require 'spec/rails/matchers/associations'
   require 'spec/rails/matchers/validations'
